d47951b9c1059aa456d519889855df35faccee06
twinfate <gaziiz.2gn@gmail.com>
2021-07-26 17:51:53 +0600

    Init Interpreter Project

---
+++ behavioral/interpreter/src/OrExpression.java
@@ -0,0 +1,18 @@
+/**
+ * Daddy: GM
+ * BirthDate: 26.07.2021
+ */
+public class OrExpression implements Expression {
+    private Expression expression1;
+    private Expression expression2;
+
+    public OrExpression(Expression expression1, Expression expression2) {
+        this.expression1 = expression1;
+        this.expression2 = expression2;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return expression1.interpret(context) || expression2.interpret(context);
+    }
+}
---
+++ behavioral/interpreter/src/TerminalExpression.java
@@ -0,0 +1,19 @@
+/**
+ * Daddy: GM
+ * BirthDate: 26.07.2021
+ */
+public class TerminalExpression implements Expression {
+    String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        if (context.contains(data)) {
+            return true;
+        }
+        return false;
+    }
+}
