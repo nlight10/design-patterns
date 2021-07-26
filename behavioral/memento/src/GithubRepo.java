a9b0bf322f22c730aa67bd740c237770ed2cef34
nlight10 <gaziz.2gn@gmail.com>
2021-07-26 22:37:19 +0600

    Init Memento Project

---
+++ behavioral/memento/src/GithubRepo.java
@@ -0,0 +1,11 @@
+public class GithubRepo {
+    private Save save;
+
+    public Save getSave() {
+        return save;
+    }
+
+    public void setSave(Save save) {
+        this.save = save;
+    }
+}
